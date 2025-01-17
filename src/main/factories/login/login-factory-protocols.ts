1057a863ce3faa46db7ea9cd2d4782fcce0ff586
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-01-17 11:07:54 -0300

    refactor: remove log controller decorator

--- src/main/factories/login/login-factory-protocols.ts
+++ src/main/factories/login/login-factory-protocols.ts
@@ -3,7 +3,6 @@ export * from "../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter";
 export * from "../../../infra/cryptography/jwt-adapter/jwt-adapter";
 export * from "../../../data/usecases/authentication/db-authentication";
 export * from "../../../presentation/controllers/login/login-controller";
-export * from "../../decorators/log-controller";
 export * from "../../../infra/axios/account/account-manager-repository";
 export * from "../../../presentation/helpers/validators/email-validation";
 export * from "../../../presentation/helpers/validators/required-field-validation";
