ea271a313830b9b974b7b334c0b77bb77cfd692c
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-02-20 12:36:55 -0300

    refactor: add verify-token-protocols

---
+++ src/presentation/controllers/auth/verify-token-protocols.ts
@@ -0,0 +1,5 @@
+export * from "./verify-token-controller";
+export * from "../../protocols";
+export * from "../../../domain/usecases/verify-decoded-token";
+export * from "../../helpers/http/http-helper";
+export * from "./../../../domain/usecases/verify-decoded-token";
