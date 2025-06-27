ec471d0eb98acd4ad520fe4816d2c57d7713acd8
Tony Sandro <tonysduarte101@gmail.com>
2025-06-27 01:24:25 -0300

    fix: update jwtSecret to use environment variable

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   baseUrl: process.env.BASE_URL || "http://localhost:5053/api",
   port: process.env.PORT || 5050,
-  jwtSecret: "be1bf1abba86598a9a771db2db174293086f82e3c56904f84a6b8109f117e1b6",
+  jwtSecret: process.env.JWT_SECRET,
 };
