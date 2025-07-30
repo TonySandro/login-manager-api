1b7be91086b9270de2dfd27352aab1fd7d8e5100
Tony Sandro <tonysduarte101@gmail.com>
2025-07-30 10:29:15 -0300

    feat: implement health check controller and routes

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 import { adaptRoute } from "../adapters/express/express-route-adapter";
 import { makeLoginController } from "../factories/login/login-factory";
 import { makeVerifyTokenController } from "../factories/auth/auth-factory";
+import { makeHealthCheckController } from "../factories/health/health-check";
 
 export default (router: Router): void => {
   router.post("/login", adaptRoute(makeLoginController()));
   router.get("/auth", adaptRoute(makeVerifyTokenController()));
+  router.get("/health", adaptRoute(makeHealthCheckController()));
 };
